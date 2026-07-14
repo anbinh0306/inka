92ef6337c852fd0834f4272a1b0d8bb94513b04d
An Binh <abinh79@gmail.com>
2026-07-14 23:53:41 +0700

    upgrade

---
+++ script.js
@@ -0,0 +1,16 @@
+const discordBtn = document.querySelector("#discord-btn");
+const editBtn = document.querySelector("#edit-btn");
+
+discordBtn.addEventListener("click", function() {
+
+    window.location.href = "https://discord.gg/2tTEUtEha4";
+
+});
+
+editBtn.addEventListener("click", function() {
+
+    window.location.href = "editor/edit.html";
+
+});
+
+console.log("Dep trai vc xD");
