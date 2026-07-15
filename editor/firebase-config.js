6ce5ca4fe0f962502c74d8f7704aa053c9cab2fe
An Binh <abinh79@gmail.com>
2026-07-15 23:13:51 +0700

    upgrade

---
+++ editor/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKmco9Bl8jpfbMnEFhcLHEoDPTANBYOdk",
+  authDomain: "inka-879a7.firebaseapp.com",
+  projectId: "inka-879a7",
+  storageBucket: "inka-879a7.firebasestorage.app",
+  messagingSenderId: "671804393691",
+  appId: "1:671804393691:web:b39fc4fe83a541b2ffa4fe"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
